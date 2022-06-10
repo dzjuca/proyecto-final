3356fc55c386fbe04a7fc6d96be9477fa51aee99
Daniel Z. Juca <daniel3959@hotmail.com>
2022-06-10 18:59:36 -0500

    Se modifican rutas y componentes, se empieza a trabajr en el login

--- pulso/src/app/app-routing.module.ts
+++ pulso/src/app/app-routing.module.ts
@@ -26,6 +26,14 @@ const routes: Routes = [
     path: '',
     redirectTo: 'intro',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
   }
 ];
 
--- pulso/src/app/components/components.module.ts
+++ pulso/src/app/components/components.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { IonicModule } from '@ionic/angular';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, InputComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, InputComponent]
 })
 export class ComponentsModule { }
