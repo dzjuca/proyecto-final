d184c20dd833d85bd9a8ba53de23f9aab06b2b45
Daniel Z. Juca <daniel3959@hotmail.com>
2022-06-15 21:39:08 -0500

    Se añade RouterModule al módulo de componentes

--- pulso/src/app/components/components.module.ts
+++ pulso/src/app/components/components.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { InputComponent } from './input/input.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, InputComponent],
+  declarations: [HeaderComponent, InputComponent, MenuComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    RouterModule
   ],
-  exports: [HeaderComponent, InputComponent]
+  exports: [HeaderComponent, InputComponent, MenuComponent]
 })
 export class ComponentsModule { }
