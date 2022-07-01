8f5dae7dd5307e7a5ed58702938f9993548bca70
Daniel Z. Juca <daniel3959@hotmail.com>
2022-06-30 19:51:53 -0500

    Se termina toda la parte visual de la pagina de perfil

--- pulso/src/app/pages/perfil/perfil.page.ts
+++ pulso/src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  mode:string;
+  disabled:boolean;
+  user:any ={
+    name:"Daniel Z. Juca",
+    password:"1234",
+    email:"dzjuca@gmail.com",
+    username:"__daniel82__",
+    birthday:"05-06-1982",
+    phone:"0984746267",
+    suscription: "Mensual",
+    avatar:"/assets/img/avatar/ian-avatar.png"
+  }
+
+  constructor() {
+    this.mode = 'view';
+    this.disabled = true;
+  }
 
   ngOnInit() {
   }
+
+  editPerfil(){
+    this.mode = 'edit';
+    this.disabled = false;
+  }
+
+  save(){
+    this.mode = 'view';
+    this.disabled = true;
+  }
+
+  cancel(){
+    this.mode = 'view';
+    this.disabled = true;
+  }
+
+
 
 }
